a5882fe2c4066c853f68e059c39f19e8d37d003b
EduuFerreira <eduardosilvaferreira4@gmail.com>
2025-11-18 20:46:43 -0300

    Create: Criação da aplicação

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
 from flask import render_template
 from . import main
+from ..models import User
 
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    users = User.query.all()
+    return render_template('index.html', users=users)
