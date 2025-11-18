dc4cd5b8ebad3946f4f767b2e3cc8b2448fcae8f
Eduardo Ferreira <eduardosilvaferreira4@gmail.com>
2025-11-18 20:55:40 -0300

    Reversão do commit

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,7 @@
 from flask import render_template
 from . import main
-from ..models import User
 
 
 @main.route('/')
 def index():
-    users = User.query.all()
-    return render_template('index.html', users=users)
+    return render_template('index.html')
